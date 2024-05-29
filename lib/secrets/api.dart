57057d5eb41572efc3e474588c3196adcf57b92e
haadiyaa <haadiyact@gmail.com>
2024-05-29 11:20:38 +0530

    add gitignore

--- lib/secrets/api.dart
+++
@@ -1,6 +0,0 @@
-class APIEndPoints{
-  final String cityUrl="https://api.openweathermap.org/data/2.5/weather?q=";
-  final units="&units=metric";
-  final apiKey="6e85118d0f380c857ca86591512dcdde";
-final aiKey="6e85118d0f380c857ca86591512dcdde";
-}
