9747c8ce90d456e8e6fc1f26a0cb15620bf00cc5
haadiyaa <haadiyact@gmail.com>
2024-05-29 10:59:11 +0530

    added gitignore

--- lib/secrets/api.dart
+++ lib/secrets/api.dart
@@ -1,5 +1,4 @@
 class APIEndPoints{
-  //https://api.openweathermap.org/data/2.5/weather?q=calicut&appid=6e85118d0f380c857ca86591512dcdde&units=metric
   final String cityUrl="https://api.openweathermap.org/data/2.5/weather?q=";
   final units="&units=metric";
   final apiKey="6e85118d0f380c857ca86591512dcdde";
